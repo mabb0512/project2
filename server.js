e3b1cbeeb0eef1554c1ae6d91e794fa5b50c560f
mabb0512 <mariana.bravo@gmail.com>
2018-12-11 20:24:15 -0500

    addding mvc folders

--- server.js
+++ server.js
@@ -11,12 +11,18 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 // Requiring our models for syncing
 var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true }).then(function() {
