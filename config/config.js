b8131f7a3d1f8282808d781ef338b3488f83e847
mabb0512 <mariana.bravo@gmail.com>
2018-12-22 10:13:23 -0500

    confi.js

--- config/config.js
+++ config/config.js
@@ -14,8 +14,11 @@ var config = {
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable":"JAWS_DB_URL",
-    "dialect": "mysql"
+    "use_env_variable":"x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "database": "moy34g2efz3achlq",
+    "dialect": "mysql",
+    "username": "b9xmzlxt38e1s5ys",
+    "password": "dg07nsejycegkmnv",
   }
 };
 
